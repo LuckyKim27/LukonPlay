d6568e6fdecb46ea0bc221d55a25908cd9fcc163
Lucky Kim <lukikim03@gmail.com>
2026-07-21 00:18:26 +0700

    Update Supabase client initialization method

--- js/config.js
+++ js/config.js
@@ -3,5 +3,5 @@ const SUPABASE_URL = "https://vecatfociqytqeilggfb.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_a9Lguh5-hL1KthRZGE492Q_yyVh6C9z";
 
 // Inisialisasi Supabase Client
-const { createClient } = supabase;
+const { createClient } = window.supabase;
 const dbSupabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
